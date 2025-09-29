caa5470474235819112bd860cd5ae4d624aaa8cb
thhn_trroq <133735405+Troqcoder-211@users.noreply.github.com>
2025-09-29 09:55:41 +0700

    update cmt

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import mediapipe as mp
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
+
 def run(use_flip=False):
     cap = cv2.VideoCapture(0)  # mở camera mặc định
 
@@ -13,7 +14,7 @@ def run(use_flip=False):
         static_image_mode=False,
         max_num_hands=2,
         min_detection_confidence=0.5,
-        min_tracking_confidence=0.5
+        min_tracking_confidence=0.5,
     ) as hands:
 
         while cap.isOpened():
@@ -22,9 +23,7 @@ def run(use_flip=False):
                 print("❌ Không lấy được frame từ camera")
                 break
 
-            # Chỉ lật khi cần
-            # if use_flip:
-            #     frame = cv2.flip(frame, 1)
+            frame = cv2.flip(frame, 1)
 
             # Chuyển sang RGB cho mediapipe
             rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -36,19 +35,18 @@ def run(use_flip=False):
             if results.multi_hand_landmarks:
                 for hand_landmarks in results.multi_hand_landmarks:
                     mp_drawing.draw_landmarks(
-                        frame,
-                        hand_landmarks,
-                        mp_hands.HAND_CONNECTIONS
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
                     )
 
             # Hiển thị
             cv2.imshow("HandLang - Mediapipe Demo", frame)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 
     cap.release()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     run()
